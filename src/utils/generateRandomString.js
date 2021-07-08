d6c25f45cbf4bbf7732a3ff7ba304ab045b84456
marco79423 <marco79423@gmail.com>
2021-07-08 21:40:39 +0800

    新增 generateRandomString 的註解

--- src/utils/generateRandomString.js
+++ src/utils/generateRandomString.js
@@ -1,6 +1,12 @@
 export const DEFAULT_LENGTH = 6
 export const DEFAULT_CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
+/**
+ * 產生随機亂數
+ * @param {number} config.length - 字串長度
+ * @param {number} config.charset - 字元集
+ * @returns {string}
+ */
 export default function generateRandomString(config) {
   const {length = DEFAULT_LENGTH, charset = DEFAULT_CHARSET} = config || {}
 
