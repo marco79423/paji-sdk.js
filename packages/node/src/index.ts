d426467356876aa32f7b40a681422498b494829c
Marco Li <marco_li@paradise-soft.com.tw>
2021-09-13 16:56:06 +0800

    新增 MongoClient

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,3 +1,4 @@
 export {default as NATSClient} from './NATSClient'
 export {default as NATSStreamingClient} from './NATSStreamingClient'
 export {default as RedisClusterClient} from './RedisClusterClient'
+export {default as MongoClient} from './MongoClient'
