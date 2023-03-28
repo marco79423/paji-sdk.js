0091462b4faf66ec6afbcb9bb677ab333182c752
Marco Li <marco_li@paradise-soft.com.tw>
2023-03-28 14:23:39 +0800

    feat: remove next-lib, node

--- packages/node/src/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as NATSClient} from './NATSClient'
-export {default as NATSStreamingClient} from './NATSStreamingClient'
-export {default as RedisClusterClient} from './RedisClusterClient'
-export {default as MongoClient} from './MongoClient'
